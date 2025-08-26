01bc3b89ec6ef856da10f65a272bd852f05a36c2
pippinbarr <pippin.barr@gmail.com>
2025-08-26 17:05:00 -0400

    Just some absolute CLASSIC fails on canvas scaling
    - Why do I bother?
    - Seriously why? It's such an uninteresting problem to spend time on, and yet here we fuckin are.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,42 @@
 */
 
 "use strict";
+
+let canvas = undefined; // A reference to the canvas for scaling with CSS
+
+
+/**
+ * Create the canvas, probably more
+ */
+function setup() {
+    canvas = createCanvas(9 * 64, 16 * 64);
+    windowResized();
+    console.log(width, height);
+}
+
+/**
+ * For now we're just making the canvas visible while I mess with CSS
+ */
+function draw() {
+    background(0);
+}
+
+function windowResized() {
+    // canvas.style("display", "inline-block");
+
+    // Compare the ratios to figure out which was to stretch the canvas proportionally
+    if (windowWidth / windowHeight > 9 / 16 && windowHeight / windowWidth < 16 / 9) {
+        // Prioritize the vertical dimension
+        canvas.style("height", "100vh");
+        canvas.style("width", "auto");
+    }
+    else {
+        // Prioritize the horizontal
+        canvas.style("width", "100vw");
+        canvas.style("height", "auto");
+    }
+
+    console.log(window.innerWidth, window.innerHeight);
+    console.log(windowWidth, windowHeight);
+    console.log(canvas.width, canvas.height);
+}
