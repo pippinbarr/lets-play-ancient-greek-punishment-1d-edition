67ad67e87e552c14a75780e10ab7ded0f9a4a6e9
pippinbarr <pippin.barr@gmail.com>
2025-10-01 11:35:42 -0400

    Worked on Sisyphus, dropped tweens
    - I gave up on tweens (for now?) as a way to represent motion and have tried switching to some "simple" physics-based representation of what's going on
    - But predictably there's no such thing as simple physics for me, even in one dimension
    - So it does some of the stuff I want and looks cool in some of the ways I want, but the real physicality of the boulder isn't there yet... you can push it and it rolls up, which I really this works well, but it's all still a bit skate-y and I fear it's a combination of procedures AND numbers that's going to make it hard to get right
    - But getting it right does seem core to whatever the point of this game is... I'm increasingly thinking about the Jankovics animation and the idea of effort in that... can I achieve that on a maths number line? I'm sure it can be done, can *I* do it? Less clear. Less clear.
    - Anyway, I'll call today progress and come back to it with fresher eyes another time... we're some of the way there and much of it "makes sense", it's mostly some of the interaction moments and the sheer numbers game of it?
    - I do think the other minigames are easier because they *don't* have this direct physical interaction between points, so I'll remain hopeful those ones are less strenuous

--- js/Sisyphus.js
+++ js/Sisyphus.js
@@ -26,21 +26,37 @@ class Sisyphus extends State {
         };
         // The point that is Sisyphus
         this.sisyphus = {
-            x: 0.4, // How far along the line he is
+            x: 0, // How far along the line he is
+            vx: 0,
+            maxVX: 1,
+            ax: 0,
+            maxAX: 0.1,
+            pushVX: 0.005,
+            walkAX: 0.001,
+            fatigue: -0.0001,
             size: 0.03, // The size of his dot
             label: "S",
             fill: "#000000",
             alpha: 255,
-            progress: 1
+            progress: 0,
+            visible: true,
+            contact: false,
+            walking: false,
         };
         // The point that is the boulder
         this.boulder = {
-            x: 0,
+            x: 0.2,
+            vx: 0,
+            maxVX: 1,
+            ax: 0,
+            maxAX: 0.1,
+            gravity: -0.00001,
             size: 0.03,
             label: "B",
             fill: "#a86832",
             alpha: 255,
-            progress: 1
+            progress: 0,
+            visible: true
         };
 
         this.pointsData = [this.sisyphus, this.boulder];
@@ -59,12 +75,52 @@ class Sisyphus extends State {
      * Once per frame, updates it all
      */
     update() {
+        console.log(this.sisyphus.x, this.sisyphus.vx, this.sisyphus.ax);
         // Display the line and points
         this.display();
 
-        // Ugly but updating sisyphus and the boulder for "gravity"
-        this.sisyphus.x = constrain(this.sisyphus.x - 0.001, 0, 1);
-        this.boulder.x = this.sisyphus.x + (this.sisyphus.size / 2 + this.boulder.size / 2);
+        this.moveSisyphus();
+        this.moveBoulder();
+
+        // If the boulder touches sisyphus on this frame then it's in contact
+        // and transfers its acceleration to him (e.g. should push him down the hill depending on how fast it was moving at the time)
+        // And so the boulder stops (he stops it? Hmmm...)
+        if (this.boulder.x < this.sisyphus.x + this.sisyphus.size) {
+            this.boulder.x = this.sisyphus.x + this.sisyphus.size;
+            this.sisyphus.contact = true;
+            // this.sisyphus.ax += this.boulder.ax;
+            this.boulder.vx = 0;
+            this.boulder.ax = 0;
+        }
+        // If it's not touching sisyphus and reached the top it should stop
+        // Unclear how useful this is...
+        else if (this.boulder.x === 1) {
+            this.boulder.vx = 0;
+            this.boulder.ax = 0;
+        }
+    }
+
+    moveSisyphus() {
+        // Sisyphus loses acceleration from fatigue (e.g. accelerates 
+        // backwards) -- this is what we're fighting with clicking when
+        // he's walking up the hill and why we have to keep clicking to give
+        // him forwards acceleration
+        this.sisyphus.ax = constrain(this.sisyphus.ax + this.sisyphus.fatigue, -this.sisyphus.maxAX, this.sisyphus.maxAX);
+        // Sisyphus moves according to his acceleration in the usual way
+        // Except that we don't want him to slide backwards unless he's in contact with the boulder right?
+        this.sisyphus.vx = constrain(this.sisyphus.vx + this.sisyphus.ax, 0, this.sisyphus.maxVX);
+        // Sisyphus moves in the standard way with current velocity
+        this.sisyphus.x = constrain(this.sisyphus.x + this.sisyphus.vx, 0, 1);
+    }
+
+    moveBoulder() {
+        // Boulder loses acceleration from gravity
+        this.boulder.ax = constrain(this.boulder.ax + this.boulder.gravity, -this.boulder.maxAX, this.boulder.maxAX);
+        // Boulder velocity changes according to acceleration in the normal 
+        // way
+        this.boulder.vx = constrain(this.boulder.vx + this.boulder.ax, -this.boulder.maxVX, this.boulder.maxVX);
+        // Boulder moves according to velocity in th enormal way
+        this.boulder.x = constrain(this.boulder.x + this.boulder.vx, 0, 1);
     }
 
     /**
@@ -78,7 +134,19 @@ class Sisyphus extends State {
      * What happens when you click? A little push.
      */
     mousePressed() {
-        this.sisyphus.x = constrain(this.sisyphus.x + 0.015, 0, 1);
-        this.boulder.x = this.sisyphus.x + (this.sisyphus.size / 2 + this.boulder.size / 2);
+        // this.sisyphus.x = constrain(this.sisyphus.x + 0.015, 0, 1);
+        // this.boulder.x = this.sisyphus.x + (this.sisyphus.size / 2 + this.boulder.size / 2);
+
+        // If sisyphus is touching the boulder on click, then push it
+        // up the hill using velocity
+        if (this.sisyphus.contact) {
+            this.boulder.vx = this.sisyphus.pushVX;
+            this.sisyphus.contact = false;
+        }
+        // If not, then sisyphus should accelerate up the hill based
+        // on the click
+        else {
+            this.sisyphus.ax = constrain(this.sisyphus.ax + this.sisyphus.walkAX, 0, this.sisyphus.maxAX);
+        }
     }
 }
--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function setup() {
     windowResized();
 
     // Starting state
-    state = new Danaids();
+    state = new Sisyphus();
 }
 
 /**
